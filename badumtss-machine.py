882dbf85d468428720fa644dea877f312525fd6c
Jacek Konieczny <jajcus@jajcus.net>
2016-10-20 18:47:17 +0200

    EV_ABS events handling for dynamic velocities
    Doesn't feel exactly right, but works.

--- badumtss-machine.py
+++ badumtss-machine.py
@@ -29,8 +29,10 @@ import time
 import logging
 import re
 import asyncio
+import math
+import sys
 
-from configparser import ConfigParser
+from configparser import ConfigParser, ExtendedInterpolation
 
 from functools import partial
 from queue import Queue, Empty
@@ -38,7 +40,7 @@ from queue import Queue, Empty
 import jack
 import evdev
 
-from evdev.ecodes import EV_KEY
+from evdev.ecodes import EV_KEY, EV_ABS
 
 logger = logging.getLogger()
 
@@ -139,37 +141,128 @@ class JackPlayer(object):
                     ])
 
 class EventHandler(object):
-    def __init__(self, device, settings, player):
+    def __init__(self, device, key, settings, player):
         self._device = device
         self._settings = settings
         self._player = player
-    def get_velocity(self, event):
+    def get_velocity(self):
         return int(self._settings.get("velocity", 127))
-    def event_value(self, event):
+    def interpret_event(self, event):
         raise NotImplementedError
     def process(self, event):
-        event_value = self.event_value(event)
-        if event_value == "ignore":
+        interpret_event = self.interpret_event(event)
+        if interpret_event == "ignore":
             return
         if "note" in self._settings:
             note = int(self._settings["note"])
             channel = int(self._settings.get("channel", 1))
-            velocity = self.get_velocity(event)
-            if event_value == "on":
+            velocity = self.get_velocity()
+            if interpret_event == "on":
                 logger.debug("  note on: %r, %r, %r", channel, note, velocity)
                 self._player.note_on(channel, note, velocity)
-            elif event_value == "off":
+            elif interpret_event == "off":
                 logger.debug("  note off: %r, %r, %r", channel, note, velocity)
                 self._player.note_off(channel, note, velocity)
 
 class KeyEventHandler(EventHandler):
-    def event_value(self, event):
+    def interpret_event(self, event):
         if event.keystate == event.key_down:
             return "on"
         elif event.keystate == event.key_up:
             return "off"
         else:
             return "ignore"
+
+class AbsEventHandler(EventHandler):
+    def __init__(self, device, key, settings, player):
+        super().__init__(device, key, settings, player)
+        self._last_value = None
+        self._last_value_ts = None
+        self._min = None
+        self._max = None
+        self._thres_low = None
+        self._thres_high = None
+        self._velocity = None
+        self._velocity_coeff = float(settings.get("velocity_coeff", 2.0))
+        etype, ecode  = key
+        abs_caps = device.capabilities(absinfo=True)[etype]
+        for code, absinfo in abs_caps:
+            if code == ecode:
+                break
+        else:
+            logger.error("Cannot retrieve absinfo for %r", ecode)
+            return
+        self._min = absinfo.min
+        self._max = absinfo.max
+        self._range = absinfo.max - absinfo.min
+        def val_to_abs(val):
+            if not val.endswith("%"):
+                return float(val)
+            pcent = float(val[:-1])
+            return self._min + pcent * (self._max - self._min) / 100.0
+        if "thres_low" in settings:
+            self._thres_low = val_to_abs(settings["thres_low"])
+        else:
+            self._thres_low = self._min
+        if "thres_high" in settings:
+            self._thres_high = val_to_abs(settings["thres_high"])
+        else:
+            self._thres_high = self._max
+
+    def get_velocity(self):
+        if self._velocity is not None:
+            return self._velocity
+        else:
+            return super().get_velocity()
+
+    def _compute_velocity(self, value, event_ts):
+        if not self._range:
+            self._velocity = None
+            return
+        rel_change = float(value - self._last_value) / self._range
+        if rel_change > 0 and self._last_value < self._thres_low:
+            # the low value could be collected before the move started
+            velocity = math.inf
+        elif rel_change < 0 and self._last_value > self._thres_high:
+            # the high value can be collected before the move started
+            velocity = math.inf
+        else:
+            time_change = event_ts - self._last_value_ts
+            velocity = abs(rel_change / time_change)
+        logger.debug("unscaled velocity: %f", velocity)
+        if velocity != math.inf:
+            velocity = int(velocity * self._velocity_coeff)
+        if velocity < 0:
+            self._velocity = 0
+        elif velocity > 127:
+            self._velocity = 127
+        else:
+            self._velocity = velocity
+
+    def interpret_event(self, event):
+        value = event.event.value
+        event_ts = event.event.timestamp()
+        if self._last_value is None:
+            result = "ignore"
+        elif value > self._last_value:
+            # rising
+            if value > self._thres_high and self._last_value < self._thres_high:
+                result = "on"
+                self._compute_velocity(value, event_ts)
+            else:
+                result = "ignore"
+        elif value < self._last_value:
+            # falling
+            if value < self._thres_low and self._last_value > self._thres_low:
+                result = "off"
+                self._compute_velocity(value, event_ts)
+            else:
+                result = "ignore"
+        else:
+            result = "ignore"
+        self._last_value = value
+        self._last_value_ts = event_ts
+        return result
 
 class InputDeviceHandler(object):
     def __init__(self, config, device, player):
@@ -193,19 +286,27 @@ class InputDeviceHandler(object):
                     continue
                 handler_class = KeyEventHandler
                 ev_type = EV_KEY
+            elif section.startswith("ABS_"):
+                try:
+                    ecode = evdev.ecodes.ecodes[section]
+                except KeyError:
+                    logger.warning("Unknown axis name: %r", section)
+                    continue
+                handler_class = AbsEventHandler
+                ev_type = EV_ABS
             else:
                 continue
             settings = dict(defaults)
             settings.update(self.config[section])
-            handler = handler_class(self.device, settings, self.player)
-            self.map[ev_type, ecode] = handler
+            key = (ev_type, ecode)
+            handler = handler_class(self.device, key, settings, self.player)
+            self.map[key] = handler
 
     async def handle_events(self):
         async for event in self.device.async_read_loop():
             ev_type = event.type
             ev_code = event.code
             event = evdev.categorize(event)
-            logger.debug("%s: %s", self.device.fn, event)
             handler = self.map.get((ev_type, ev_code))
             if handler:
                 handler.process(event)
@@ -214,14 +315,16 @@ INTRO_NOTES = [38, 38, 0, 49]
 
 def play_intro(player):
     for note in INTRO_NOTES:
-        print("#")
+        print(".", end="")
+        sys.stdout.flush()
         if note:
             player.note_on(10, note, 127)
         time.sleep(0.2)
+    print()
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
-    config = ConfigParser()
+    config = ConfigParser(interpolation=ExtendedInterpolation())
     config['DEFAULT'] = {
                          "input_device": ".*",
                          "jack_connect": "",
